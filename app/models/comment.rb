9769b0d72df91b345d690f585deb2ee1e0ad771d
Hatens Chris <jayhatens@gmail.com>
2017-11-26 14:50:16 +0000

    updates on actioncable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
-class Comment < ActiveRecord::Base
+class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  scope :rating_desc, -> { order(rating: :desc) }
-  scope :rating_asc, -> { order(rating: :asc) }
+  scope :rating_desc, -> { order(rating: :desc)}
+  scope :rating_asc, -> { order(rating: :asc)}
 
-  validates :body, presence: true
+  validates :body, :user, :product, presence: true
   validates :rating, numericality: { only_integer: true }
-  
   after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
