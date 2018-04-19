05ec3319e52c8ffbfa8412846b98a738ef77f73c
Hatens Chris <jayhatens@gmail.com>
2018-04-19 17:56:50 +0000

    improvements on the app

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "admin@bb.com"
+  default from: "systemmessage@realbets254.com"
 
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-         to: 'jayhatens@gmail.com',
-         subject: "A new contact form message from #{name}")
+         to: 'support@realbets254.com',
+         subject: "#{name} On Realbets254 website")
   end
 end
